d00fbacf415699bcf4f5191ed2341f4ac9dce6ed
Jens Krumsieck <jenso666@live.de>
2020-03-22 22:53:05 +0100

    added xamarin project

---
+++ CHN.Mobile/CHN.Mobile/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using CHN.Shared;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CHN.Mobile
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public Dictionary<string, double> Theoretical { get; set; }
+        public Dictionary<string, double> Deviation { get; set; }
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SumFormula_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(SumFormula.Text) && SumFormula.Text.Length > 2)
+            {
+                SumFormulaParsed.Text = $" Parsed Sum Formula: {SumFormula.Text.Parse()} - Molecular Weight: {SumFormula.Text.MolWeight()}";
+                Theoretical = SumFormula.Text.Deviation();
+                BindableLayout.SetItemsSource(theoreticalView, Theoretical);
+            }
+        }
+
+        private void Exp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(SumFormula.Text) && SumFormula.Text.Length > 2)
+            {
+                Deviation = Theoretical.Deviation(Experimental);
+                BindableLayout.SetItemsSource(deviationView, Deviation);
+            }
+        }
+
+        private Dictionary<string, double> Experimental => new Dictionary<string, double>
+        {
+            ["C"] = expC.Text.ToDouble(),
+            ["H"] = expH.Text.ToDouble(),
+            ["N"] = expN.Text.ToDouble(),
+            ["F"] = expS.Text.ToDouble(),
+            ["S"] = expF.Text.ToDouble()
+        };
+    }
+}
